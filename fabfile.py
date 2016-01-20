1ae65d2ca8bb418b28906cb24f14e0813bdcbb01
vikas17a <vikas17a@gmail.com>
2016-01-21 01:46:21 +0530

    test

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,4 @@
-from fabric.api import *
-from fabric.operations import local as lrun, run
-from fabric.operations import  put
-import time
-import urllib2
-import ConfigParser
-import os.path
-import json
-import yaml
-from pprint import pprint
-import logging
-from fabric.colors import green, red
-from random import randint
-from fabric.context_managers import cd
-env.user = 'vikas'
-env.shell = "/bin/sh -c"
-env.use_ssh_config = True
+from fabric.api import run
 
-def localhost():
-  env.run = lrun
-  env.hosts = ['localhost']
-
-def deploy_test():
-  run('uname -a')
+def host_type():
+    run('uname -s')
